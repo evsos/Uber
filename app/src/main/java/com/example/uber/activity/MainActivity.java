f3581097d060d742eee2dff61259f994d79d2698
evsos <coderiser.evsos@gmail.com>
2020-05-01 17:06:08 +0100

    locations permissions set on MainActivity, done

--- app/src/main/java/com/example/uber/activity/MainActivity.java
+++ app/src/main/java/com/example/uber/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.uber.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +10,12 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.example.uber.R;
+import com.example.uber.helper.Permissoes;
 import com.example.uber.helper.UsuarioFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public
@@ -19,6 +25,8 @@ class MainActivity extends AppCompatActivity {
     private RelativeLayout rLayout;
     private Button btnLogin;
     private Button btnRegister;
+    private FirebaseAuth auth;
+    private String[] permissoes = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected
@@ -27,6 +35,12 @@ class MainActivity extends AppCompatActivity {
         setContentView (R.layout.activity_main);
 
         getSupportActionBar ().hide ();
+
+        //validar permissoes de acesso a localizacao do usuario
+        Permissoes.validarPermissoes (permissoes,MainActivity.this, 1);
+
+        /*auth = FirebaseConfig.getFirebaseAuth ();
+        auth.signOut ();*/
 
         ivUber = findViewById (R.id.ivUber);
         rLayout = findViewById (R.id.rLayout);
@@ -49,5 +63,45 @@ class MainActivity extends AppCompatActivity {
         super.onStart ();
 
         UsuarioFirebase.redirectLoggedInUser (MainActivity.this);
+    }
+
+    @Override
+    public
+    void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult (requestCode, permissions, grantResults);
+
+        for(int permissaoResultado : grantResults){
+
+            if (permissaoResultado == getPackageManager().PERMISSION_DENIED){
+                    alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    public void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder (this);
+        builder.setTitle ("Permissoes negadas");
+        builder.setMessage ("Tem de aceitar as permissoes de acesso à localizacão");
+        builder.setCancelable (false);
+        builder.setPositiveButton ("Confirmar", new DialogInterface.OnClickListener () {
+            @Override
+            public
+            void onClick (DialogInterface dialog, int which) {
+                finish ();
+            }
+        });
+
+        AlertDialog dialog=builder.create ();
+        dialog.show ();
+
+
+
+
+
+
+
+
+
+
     }
 }
