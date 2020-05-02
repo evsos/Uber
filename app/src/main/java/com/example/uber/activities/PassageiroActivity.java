d9d61332aeec6cfcb8f741e6183bb63e340469eb
evsos <coderiser.evsos@gmail.com>
2020-05-02 17:18:36 +0100

    definir endereco de destino e confirmar esse endereco, done

--- app/src/main/java/com/example/uber/activities/PassageiroActivity.java
+++ app/src/main/java/com/example/uber/activities/PassageiroActivity.java
@@ -2,16 +2,22 @@ package com.example.uber.activities;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.uber.R;
 import com.example.uber.config.FirebaseConfig;
+import com.example.uber.model.Destino;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,6 +27,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
@@ -32,22 +43,17 @@ class PassageiroActivity extends AppCompatActivity implements OnMapReadyCallback
     private FirebaseAuth auth;
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private EditText etDestination;
+
+
 
     @Override
     protected
     void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_passageiro);
-        Toolbar toolbar = findViewById (R.id.toolbar);
-        toolbar.setTitle ("Iniciar uma viagem");
-        setSupportActionBar (toolbar);
 
-        auth = FirebaseConfig.getFirebaseAuth ();
-
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager ().findFragmentById (R.id.map);
-        mapFragment.getMapAsync (this);
-
+        inicializarComponentes ();
     }
 
     @Override
@@ -57,8 +63,6 @@ class PassageiroActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         getUserLocation ();
-
-
     }
 
     private
@@ -102,8 +106,70 @@ class PassageiroActivity extends AppCompatActivity implements OnMapReadyCallback
         if (ActivityCompat.checkSelfPermission (this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             locationManager.requestLocationUpdates (LocationManager.GPS_PROVIDER, 10000, 10, locationListener);
         }
+    }
 
+    public void callUber (View view){
 
+    String enderecoDestino = etDestination.getText ().toString ();
+    if (!enderecoDestino.equals ("") || enderecoDestino != null){
+
+        Address addressDestino = recuperarEndereco (enderecoDestino);
+
+        if(addressDestino != null ){
+
+            Destino destino = new Destino();
+            destino.setCidade (addressDestino.getAdminArea());
+            destino.setCep(addressDestino.getPostalCode());
+            destino.setBairro(addressDestino.getSubLocality ());
+            destino.setRua (addressDestino.getThoroughfare ());
+            destino.setNumero (addressDestino.getFeatureName ());
+            destino.setLatitude (String.valueOf (addressDestino.getLatitude ()));
+            destino.setLongitude (String.valueOf (addressDestino.getLongitude ()));
+
+            StringBuilder mensagem = new StringBuilder ();
+            mensagem.append ("Cidade:"+destino.getCidade ());
+            mensagem.append ("\nRua:"+destino.getRua ());
+            mensagem.append ("\nBairro:"+destino.getBairro());
+            mensagem.append ("\nNúmero:"+destino.getNumero ());
+            mensagem.append ("\nCep:"+destino.getCep ());
+
+            AlertDialog.Builder builder = new AlertDialog.Builder (this).setTitle ("Confirme seu endereco").setMessage (mensagem).setPositiveButton ("Confirmar", new DialogInterface.OnClickListener () {
+                @Override
+                public
+                void onClick (DialogInterface dialog, int which) {
+
+                    //salvar requisicao
+                }
+            }).setNegativeButton ("Cancelar", new DialogInterface.OnClickListener () {
+                @Override
+                public
+                void onClick (DialogInterface dialog, int which) {
+
+                }
+            });
+            AlertDialog dialog = builder.create ();
+            dialog.show ();
+        }
+
+    }
+
+    }
+
+    private Address recuperarEndereco(String endereco){
+        Geocoder geocoder = new Geocoder (this, Locale.getDefault ());
+        try {
+            List<Address> listaEnderecos = geocoder.getFromLocationName (endereco,1);
+            if(listaEnderecos!=null && listaEnderecos.size ()>0){
+                Address address = listaEnderecos.get(0);
+
+                return address;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace ();
+        }
+
+        return null;
     }
 
 
@@ -126,4 +192,24 @@ class PassageiroActivity extends AppCompatActivity implements OnMapReadyCallback
         }
         return super.onOptionsItemSelected (item);
     }
+
+private void inicializarComponentes(){
+    Toolbar toolbar = findViewById (R.id.toolbar);
+    toolbar.setTitle ("Iniciar uma viagem");
+    setSupportActionBar (toolbar);
+
+    //inicializar componentes
+
+    etDestination =findViewById (R.id.etDestination);
+
+
+    auth = FirebaseConfig.getFirebaseAuth ();
+
+
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager ().findFragmentById (R.id.map);
+    mapFragment.getMapAsync (this);
+
+
+
+}
 }
